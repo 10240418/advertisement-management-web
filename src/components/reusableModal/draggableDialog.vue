9cb579246c0a25266c4a8b7d4f227331e4c3f2ce
10240418 <liu2021632245@163.com>
2024-07-23 22:06:32 +0800

    弹窗多开,除去遮罩层,能点击底层代码

--- src/components/reusableModal/draggableDialog.vue
+++ src/components/reusableModal/draggableDialog.vue
@@ -1,33 +1,71 @@
 <template>
-    <el-button plain @click="dialogVisible = true">
-      Open a draggable Dialog
-    </el-button>
+  <el-button plain @click="dialogVisible = true">
+    Open a draggable Dialog
+  </el-button>
+
+  <el-button plain @click="dialogOverflowVisible = true">
+    Open a overflow draggable Dialog
+  </el-button>
+
+  <el-dialog 
+    v-model="dialogVisible" 
+    title="Tips" 
+    width="500" 
+    draggable
+    :modal="false"
+    :close-on-click-modal="false"
+
+     modal-class="dialog_class"
+  >
+    <span>It's a draggable Dialog</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+
+  <el-dialog
+    v-model="dialogOverflowVisible"
   
-   
-  
-    <el-dialog
-      v-model="dialogVisible"
-      title="Tips" width="500"
-      draggable
-      :modal=false>
-      <span>It's a draggable Dialog</span>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button @click="dialogVisible = false">Cancel</el-button>
-          <el-button type="primary" @click="dialogVisible = false">
-            Confirm
-          </el-button>
-        </div>
-      </template>
-    </el-dialog>
-  
+    destroy-on-close
+    title="Tips"
+    width="500"
+    draggable
+    :modal="false"
+    :close-on-click-modal="false"
+    
+     modal-class="dialog_class"
+  >
+    <span>It's a overflow draggable Dialog</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogOverflowVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogOverflowVisible = false">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const dialogVisible = ref(false)
+const dialogOverflowVisible = ref(false)
+</script>
+<style>
+
+.dialog_class {
+  pointer-events: none;
+}
  
-  </template>
-  
-  <script lang="ts" setup>
-  import { ref } from 'vue'
-  
-  const dialogVisible = ref(false)
- 
-  </script>
-  
+.el-dialog {
+  pointer-events: auto;
+}
+
+</style>
