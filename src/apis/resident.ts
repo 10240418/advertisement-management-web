2a774114667c1c590b998759141c585ad2f7d304
10240418 <liu2021632245@163.com>
2024-07-29 21:32:00 +0800

    網關,住戶apis

--- src/apis/resident.ts
+++ src/apis/resident.ts
@@ -1,55 +1,48 @@
 import request from '.'
 
+
+
 export const fetchResidents = (query: any) => {
   return request({
-    url: `/admin/resident`,
+    url: `/admin/resident_user`,
     method: 'get',
     params: query,
   })
 }
+// pageNum pageSize
 
 export const addResident = (data: any) => {
   return request({
-    url: `/admin/resident`,
+    url: `/admin/resident_user`,
     method: 'post',
     data,
   })
 }
-
-export const activateResident = (id: number | string) => {
-  return request({
-    url: `/admin/resident/${id}/activate`,
-    method: 'put',
-  })
-}
-
-export const deactivateResident = (id: number | string) => {
-  return request({
-    url: `/admin/resident/${id}/deactivate`,
-    method: 'put',
-  })
-}
-
-export const addStaffAccountPermission = (data: any) => {
-  return request({
-    url: `/admin/resident/permission`,
-    method: 'post',
-    data,
-  })
-}
-
-export const deleteStaffAccountPermission = (data: any) => {
-  return request({
-    url: `/admin/resident/permission`,
-    method: 'delete',
-    data,
-  })
-}
+// {
+//   "name": "Leo",
+//   "email": "dreamskyll@foxmail.com"
+// }
 
 export const deleteResident = (data: any) => {
   return request({
-    url: `/admin/resident`,
+    url: `/admin/resident_user/reset_password`,
     method: 'delete',
     data,
   })
 }
+// {
+//   "id": 1,
+//   "password": "123456"
+// }
+
+export const updateResident = (data: any) => {
+  return request({
+    url: `/admin/resident_user`,
+    method: 'put',
+    data,
+  })
+}
+// {
+//   "id": 1,
+//   "active": false
+// }
