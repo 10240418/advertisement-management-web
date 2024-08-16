09309ca08fc86e4946e6ceca3cbea0feeac8a56e
10240418 <liu2021632245@163.com>
2024-08-16 14:40:42 +0800

    detail card组件

--- src/components/cards/DetailCard.vue
+++ src/components/cards/DetailCard.vue
@@ -1,84 +1,33 @@
 <template>
     <VaCard>
-        <div>
-            <VaCardTitle>{{labels[0]}}</VaCardTitle>
-            <VaCardContent>
-                <!-- no object in dataObjects -->
-                <div class="flex flex-row gap-2">
-                    <div class="flex flex-col">
-                        <div v-for="(label,index) in labels">
-                            <VaListLabel class="flex flex-start">{{label}}</VaListLabel>
-                        </div>
-                    </div>
-                    <div class="flex flex-col">
-                        <div v-for="(data,index) in datas">
-                            <VaListLabel class="flex flex-start">{{data}}</VaListLabel>
-                        </div>
-                    </div>
-                </div>
-
-                <div v-if="dataObjectsShowInCard.length > 0" class="flex flex-row gap-2">
-                    <div class="flex flex-col">
-                        <div v-for="(label,index) in labelsObjectShowInCard">
-                            <VaListLabel class="flex flex-start">{{label}}</VaListLabel>
-                        </div>
-                    </div>
-                    <div class="flex flex-col">
-                        <div v-for="(data,index) in dataObjectsShowInCard">
-                            <VaListLabel class="flex flex-start">{{data.name}}</VaListLabel>
-                            <VaPopover color="backgroundSecondary" trigger="click"
-                            :style="{ '--va-popover-content-background-color': '#ffffff', }">
-                            <VaIcon :name="arrowDirection(isShowProperties[index])" size="20px"
-                                @click="isShowProperties[index] = !isShowProperties[index]" />
-                            <template #body>
-<!--  -->
-                            </template>
-                        </VaPopover>
-                        </div>
-                    </div>
-                    
-                </div>
-            </VaCardContent>
-        </div>
+      <div>
+        <VaCardContent>
+          <div class="flex flex-col">
+            <div v-for="(label, index) in labels" :key="index" class="flex flex-row gap-2 items-start">
+              <div class="flex justify-start items-start">
+                <VaListLabel class="flex flex-start">{{ label }}</VaListLabel>
+              </div>
+              <div class="flex absolute ml-20"> <!-- Adjusted margin for better alignment -->
+                <span :class="[indexWeight > index ? fontWeight : '', 'flex flex-start text-[16px]']">
+                  {{ datas[index] }}
+                </span>
+              </div>
+            </div>
+          </div>
+          <slot />
+        </VaCardContent>
+      </div>
     </VaCard>
-</template> 
-<script lang="ts" setup>
-import { defineProps ,PropType,watch,ref} from 'vue'
-import { useForm, useToast } from 'vuestic-ui';
-
-const props = defineProps({
-    labels:{
-        type:Array as PropType<string[]>,
-        required:true
-    },
-    datas:{
-        type:Array as PropType<any[]>,
-        required:true
-    },
-    labelsObject:{
-        type:Array as PropType<string[]>,
-        required:true
-    },
-    dataObjects:{
-        type:Array as PropType<any[]>,
-        required:true
-    }
-})
-const toast = useToast()
-const labelsShowInCard = ref<any>([])
-const labelsObjectShowInCard = ref<any>([])
-const datasShowInCard = ref<any>([])
-const dataObjectsShowInCard = ref<any>([])
-const isShowProperties = ref<boolean[]>([])
-const arrowDirection = (state: boolean) => (state ? 'va-arrow-up' : 'va-arrow-down');
-watch([props.datas,props.labels,props.dataObjects],()=>{
-    labelsShowInCard.value = props.labels
-    datasShowInCard.value = props.datas
-    dataObjectsShowInCard.value = props.dataObjects
-    labelsObjectShowInCard.value = props.labelsObject
-    isShowProperties.value = props.dataObjects.map((dataObject:any)=>{
-        return false;
-    })
-})
-
-</script>
+  </template>
+  
+  <script lang="ts" setup>
+  import { defineProps,PropType } from 'vue'
+  
+  const props = defineProps({
+    labels: { type: Array as PropType<string[]>, default: () => [] },
+    datas: { type: Array as PropType<any[]>, default: () => [] },
+    fontWeight: { type: String, default: 'font-bold' },
+    indexWeight: { type: Number, default: 0 }
+  })
+  </script>
+  
--- src/pages/task/systemTask/widgets/systemTaskDialog.vue
+++ src/pages/task/systemTask/widgets/systemTaskDialog.vue
@@ -1,147 +1,237 @@
 <template>
-    <VaCard class="w-full h-full flex">
-      <div class="w-full h-full flex flex-col ml-3">
-        <!-- Top Section -->
-        <div class="flex flex-row justify-between">
-          <div class="grid grid-cols-[1fr_3fr]" :class="{ 'gap-y-[2.2px]': !editable }">
-            <VaListLabel class="flex justify-start">ID</VaListLabel>
-            <span>{{ controlTask?.id }}</span>
-            <VaListLabel class="flex justify-start">Name</VaListLabel>
-            <span>{{ controlTask?.name }}</span>
-            <VaListLabel class="flex justify-start">Tag</VaListLabel>
-            <span>{{ controlTask?.tag }}</span>
-            <VaListLabel class="flex justify-start">Operation</VaListLabel>
-            <span>{{ controlTask?.operation }}</span>
-            <VaListLabel class="flex justify-start">Interval</VaListLabel>
-            <span>{{ controlTask?.interval }}</span>
-            <VaListLabel class="flex justify-start">Start Time</VaListLabel>
-            <span>{{ controlTask?.startAt }}</span>
-            <VaListLabel class="flex justify-start">Gateway ID</VaListLabel>
-            <div class="flex flex-row justify-between mr-3 items-center gap-3">
-              <span>{{ controlTask?.gatewayId }}</span>
-              <VaPopover color="backgroundSecondary" trigger="click" :style="{ '--va-popover-content-background-color': '#ffffff' }">
-                <VaIcon :name="arrowDirection(isGatewayCollapsed)" size="20px" @click="isGatewayCollapsed = !isGatewayCollapsed" />
-                <template #body>
-                  <div class="grid grid-cols-[1fr_3fr] border border-solid p-2 rounded-md shadow-lg">
-                    <VaListLabel class="flex justify-start">ID</VaListLabel>
-                    <span>{{ controlTask?.gateway.id }}</span>
-                    <VaListLabel class="flex justify-start">Name</VaListLabel>
-                    <span>{{ controlTask?.gateway.name }}</span>
-                    <VaListLabel class="flex justify-start">IP Address</VaListLabel>
-                    <span>{{ controlTask?.gateway.ipAddr }}</span>
-                    <VaListLabel class="flex justify-start">Remark</VaListLabel>
-                    <span>{{ controlTask?.gateway.remark }}</span>
-                    <VaListLabel class="flex justify-start">Created At</VaListLabel>
-                    <span>{{ controlTask?.gateway.createdAt }}</span>
-                  </div>
-                </template>
-              </VaPopover>
-            </div>
-            <VaListLabel class="flex justify-start">Meter ID</VaListLabel>
-            <div class="flex flex-row justify-between mr-3 items-center gap-3">
-              <span>{{ controlTask?.meterId }}</span>
-              <VaPopover color="backgroundSecondary" trigger="click" :style="{ '--va-popover-content-background-color': '#ffffff' }">
-                <VaIcon :name="arrowDirection(isMeterCollapsed)" size="20px" @click="isMeterCollapsed = !isMeterCollapsed" />
-                <template #body>
-                  <div class="grid grid-cols-[1fr_3fr] border border-solid p-2 rounded-md shadow-lg">
-                    <VaListLabel class="flex justify-start">ID</VaListLabel>
-                    <span>{{ controlTask?.meter.id }}</span>
-                    <VaListLabel class="flex justify-start">Name</VaListLabel>
-                    <span>{{ controlTask?.meter.name }}</span>
-                    <VaListLabel class="flex justify-start">Type</VaListLabel>
-                    <span>{{ controlTask?.meter.type === 0 ? 'Electricity' : 'Water' }}</span>
-                    <VaListLabel class="flex justify-start">UnitID</VaListLabel>
-                    <span>{{ controlTask?.meter.unitId }}</span>
-                    <VaListLabel class="flex justify-start">Remark</VaListLabel>
-                    <span>{{ controlTask?.meter.remark }}</span>
-                    <VaListLabel class="flex justify-start">Created At</VaListLabel>
-                    <span>{{ controlTask?.meter.createdAt }}</span>
-                  </div>
-                </template>
-              </VaPopover>
-            </div>
+  <VaCard class="w-full h-full flex">
+    <div class="w-full h-full flex flex-col ml-3">
+      <!-- Top Section -->
+      <!-- <div class="flex flex-row justify-between">
+        <div class="grid grid-cols-[1fr_3fr]" :class="{ 'gap-y-[2.2px]': !editable }">
+          <VaListLabel class="flex justify-start">ID</VaListLabel>
+          <span>{{ controlTask?.id }}</span>
+          <VaListLabel class="flex justify-start">Name</VaListLabel>
+          <span>{{ controlTask?.name }}</span>
+          <VaListLabel class="flex justify-start">Tag</VaListLabel>
+          <span>{{ controlTask?.tag }}</span>
+          <VaListLabel class="flex justify-start">Operation</VaListLabel>
+          <span>{{ controlTask?.operation }}</span>
+          <VaListLabel class="flex justify-start">Interval</VaListLabel>
+          <span>{{ controlTask?.interval }}</span>
+          <VaListLabel class="flex justify-start">Start Time</VaListLabel>
+          <span>{{ controlTask?.startAt }}</span>
+          <VaListLabel class="flex justify-start">Gateway ID</VaListLabel>
+          <div class="flex flex-row justify-between mr-3 items-center gap-3">
+            <span>{{ controlTask?.gatewayId }}</span>
+            <VaPopover color="backgroundSecondary" trigger="click"
+              :style="{ '--va-popover-content-background-color': '#ffffff' }">
+              <VaIcon :name="arrowDirection(isGatewayCollapsed)" size="20px"
+                @click="isGatewayCollapsed = !isGatewayCollapsed" />
+              <template #body>
+                <div class="grid grid-cols-[1fr_3fr] border border-solid p-2 rounded-md shadow-lg">
+                  <VaListLabel class="flex justify-start">ID</VaListLabel>
+                  <span>{{ controlTask?.gateway.id }}</span>
+                  <VaListLabel class="flex justify-start">Name</VaListLabel>
+                  <span>{{ controlTask?.gateway.name }}</span>
+                  <VaListLabel class="flex justify-start">IP Address</VaListLabel>
+                  <span>{{ controlTask?.gateway.ipAddr }}</span>
+                  <VaListLabel class="flex justify-start">Remark</VaListLabel>
+                  <span>{{ controlTask?.gateway.remark }}</span>
+                  <VaListLabel class="flex justify-start">Created At</VaListLabel>
+                  <span>{{ controlTask?.gateway.createdAt }}</span>
+                </div>
+              </template>
+            </VaPopover>
+          </div>
+          <VaListLabel class="flex justify-start">Meter ID</VaListLabel>
+          <div class="flex flex-row justify-between mr-3 items-center gap-3">
+            <span>{{ controlTask?.meterId }}</span>
+            <VaPopover color="backgroundSecondary" trigger="click"
+              :style="{ '--va-popover-content-background-color': '#ffffff' }">
+              <VaIcon :name="arrowDirection(isMeterCollapsed)" size="20px"
+                @click="isMeterCollapsed = !isMeterCollapsed" />
+              <template #body>
+                <div class="grid grid-cols-[1fr_3fr] border border-solid p-2 rounded-md shadow-lg">
+                  <VaListLabel class="flex justify-start">ID</VaListLabel>
+                  <span>{{ controlTask?.meter.id }}</span>
+                  <VaListLabel class="flex justify-start">Name</VaListLabel>
+                  <span>{{ controlTask?.meter.name }}</span>
+                  <VaListLabel class="flex justify-start">Type</VaListLabel>
+                  <span>{{ controlTask?.meter.type === 0 ? 'Electricity' : 'Water' }}</span>
+                  <VaListLabel class="flex justify-start">UnitID</VaListLabel>
+                  <span>{{ controlTask?.meter.unitId }}</span>
+                  <VaListLabel class="flex justify-start">Remark</VaListLabel>
+                  <span>{{ controlTask?.meter.remark }}</span>
+                  <VaListLabel class="flex justify-start">Created At</VaListLabel>
+                  <span>{{ controlTask?.meter.createdAt }}</span>
+                </div>
+              </template>
+            </VaPopover>
           </div>
         </div>
-            
-        <!-- Footer Section -->
-        <div class="dialog-footer">
-          <VaButton @click="cancel">Cancel</VaButton>
-        </div>
-      </div>
-    </VaCard>
-  </template>
-  
-  <script lang="ts" setup>
-  import { ref, onBeforeMount } from 'vue';
-  import { task_type } from '../../../../data/task';
-  import { useRoute } from 'vue-router';
-  import { fetchTask, updateTask} from '../../../../apis/task';
-  import editSystemTaskForm from './editSystemTaskForm.vue';
-  import { useToast } from 'vuestic-ui';
-  interface task_type_fetch {
-    id: number,
-    name: string,
-    tag: string,
-    operation: string,
-    active: boolean,
-    editable: boolean,
-    interval: number,
-    startAt: string,
-    createdAt: string,
-    uuid: string,
-    gatewayId: number,
-    meterId: number,
-    gateway: {
-      id: number,
-      createdAt: string,
-      name: string,
-      ipAddr: string,
-      remark: string
-      meters: null
-      
-    },
-    meter: {
-        id: number,
-        createdAt: string,
-        name: string,
-        type: number,
-        model: number,
-        modbusAddr: number,
-        remark: string,
-        unitId: number,
-        gatewayId: number,
-        tasks:any
-    }
-  }
-  const controlTask = ref<task_type_fetch| null>(null);
-  const route = useRoute();
-  const controlTaskId = ref(route.query.id);
-  const editable = ref(false);
-  const isGatewayCollapsed = ref(true);
-  const isMeterCollapsed = ref(true);
-  const arrowDirection = (state: boolean) => (state ? 'va-arrow-up' : 'va-arrow-down');
-  
-  const fetch = async () => {
-    if (controlTaskId.value) {
-      const res = await fetchTask({ id:controlTaskId.value})
-      controlTask.value = res.data.data;
-    }
-  };
-  
-  onBeforeMount(() => {
-    fetch();
-  });
+      </div> -->
 
-  const cancel = () => {
-    window.close();
-  };
-  </script>
-  
-  <style lang="scss">
-  .dialog-footer {
-    display: flex;
-    justify-content: flex-end;
-    margin-top: 20px;
+      <detailCard :labels="labelsProp" :datas="datasProp" :fontWeight="`font-bold`" :indexWeight="3" >
+        <div class="flex flex-col relative">
+          <!-- Gateway Section -->
+          <div class="flex flex-row w-full items-center ">
+            <VaListLabel class="flex justify-start">Gateway</VaListLabel>
+            <div class=" absolute ml-20"><span class="truncate w-[72px]">{{ controlTask?.gateway?.name }}</span>
+            </div>
+            <VaPopover class="absolute ml-52" color="backgroundSecondary" trigger="click"
+              :style="{ '--va-popover-content-background-color': '#ffffff' }">
+              <VaIcon :name="arrowDirection(isGatewayCollapsed)" size="20px"
+                @click="isGatewayCollapsed = !isGatewayCollapsed" />
+              <template #body>
+                <div class="grid grid-cols-2 gap-2 border border-solid p-2 rounded-md shadow-lg">
+                  <VaListLabel class="flex justify-start">ID</VaListLabel>
+                  <span>{{ controlTask?.gateway.id }}</span>
+                  <VaListLabel class="flex justify-start">Name</VaListLabel>
+                  <span>{{ controlTask?.gateway.name }}</span>
+                  <VaListLabel class="flex justify-start">IP Address</VaListLabel>
+                  <span>{{ controlTask?.gateway.ipAddr }}</span>
+                  <VaListLabel class="flex justify-start">Remark</VaListLabel>
+                  <span>{{ controlTask?.gateway.remark }}</span>
+                  <VaListLabel class="flex justify-start">Created At</VaListLabel>
+                  <span>{{ controlTask?.gateway.createdAt }}</span>
+                </div>
+              </template>
+            </VaPopover>
+          </div>
+
+          <!-- Meter Section -->
+          <div class="flex flex-row mr-3 items-center gap-3">
+            <VaListLabel class="flex justify-start">Meter</VaListLabel>
+            <div class=" absolute ml-20 "><span class="truncate w-[72px]">{{ controlTask?.meter?.name }}</span></div>
+
+            <VaPopover class="absolute ml-52" color="backgroundSecondary" trigger="click"
+              :style="{ '--va-popover-content-background-color': '#ffffff' }">
+              <VaIcon :name="arrowDirection(isMeterCollapsed)" size="20px"
+                @click="isMeterCollapsed = !isMeterCollapsed" />
+              <template #body>
+                <div class="grid grid-cols-[1fr_4fr] border border-solid rounded-md shadow-lg p-2">
+                  <VaListLabel class="flex justify-start">ID</VaListLabel>
+                  <span>{{ controlTask?.meter.id }}</span>
+
+                  <VaListLabel class="flex justify-start">Name</VaListLabel>
+                  <span>{{ controlTask?.meter.name }}</span>
+
+                  <VaListLabel class="flex justify-start">Type</VaListLabel>
+                  <span>{{ controlTask?.meter.type === 0 ? 'Electricity' : 'Water' }}</span>
+
+                  <VaListLabel class="flex justify-start">UnitID</VaListLabel>
+                  <span>{{ controlTask?.meter.unitId }}</span>
+
+                  <VaListLabel class="flex justify-start">Remark</VaListLabel>
+                  <span>{{ controlTask?.meter.remark }}</span>
+
+                  <VaListLabel class="flex justify-start">Created At</VaListLabel>
+                  <span>{{ controlTask?.meter.createdAt }}</span>
+
+                </div>
+              </template>
+            </VaPopover>
+          </div>
+        </div>
+      </detailCard>
+
+      <!-- Footer Section -->
+      <div class="dialog-footer">
+        <VaButton @click="cancel">Cancel</VaButton>
+      </div>
+    </div>
+  </VaCard>
+</template>
+
+<script lang="ts" setup>
+import { ref, onBeforeMount, computed } from 'vue';
+import { task_type } from '../../../../data/task';
+import { useRoute } from 'vue-router';
+import { fetchTask, updateTask } from '../../../../apis/task';
+import editSystemTaskForm from './editSystemTaskForm.vue';
+import { useToast } from 'vuestic-ui';
+import detailCard from '../../../../components/cards/detailCard.vue';
+interface task_type_fetch {
+  id: number,
+  name: string,
+  tag: string,
+  operation: string,
+  active: boolean,
+  editable: boolean,
+  interval: number,
+  startAt: string,
+  createdAt: string,
+  uuid: string,
+  gatewayId: number,
+  meterId: number,
+  gateway: {
+    id: number,
+    createdAt: string,
+    name: string,
+    ipAddr: string,
+    remark: string
+    meters: null
+
+  },
+  meter: {
+    id: number,
+    createdAt: string,
+    name: string,
+    type: number,
+    model: number,
+    modbusAddr: number,
+    remark: string,
+    unitId: number,
+    gatewayId: number,
+    tasks: any
   }
-  </style>
-  
+}
+const controlTask = ref<task_type_fetch | null>(null);
+const labelsProp = ref<string[]>(['ID', 'TAG', 'Operation', 'Interval', 'Start Time', 'Active', 'editable', 'CreatedAt', 'UUID'])
+const datasProp = computed(() => {
+  return [
+    controlTask.value?.id,
+    controlTask.value?.tag,
+    controlTask.value?.operation,
+    controlTask.value?.interval,
+    controlTask.value?.startAt,
+    controlTask.value?.active,
+    controlTask.value?.editable,
+    controlTask.value?.createdAt,
+    controlTask.value?.uuid
+  ]
+})
+const route = useRoute();
+const toast = useToast()
+const controlTaskId = ref(route.query.id);
+const editable = ref(false);
+const isGatewayCollapsed = ref(true);
+const isMeterCollapsed = ref(true);
+const arrowDirection = (state: boolean) => (state ? 'va-arrow-up' : 'va-arrow-down');
+
+
+const fetch = async () => {
+  if (controlTaskId.value) {
+    fetchTask({ id: controlTaskId.value }).then(res => {
+      controlTask.value = res.data.data;
+      console.log(res.data.data)
+    }).catch(err => {
+      toast.init({ message: err.response.data.message, color: 'danger' })
+      console.log(err)
+    })
+
+  }
+};
+
+onBeforeMount(() => {
+  fetch();
+});
+
+const cancel = () => {
+  window.close();
+};
+</script>
+
+<style lang="scss">
+.dialog-footer {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+}
+</style>
