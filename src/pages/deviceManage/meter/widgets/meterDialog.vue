21c0c8f99b55b7bdd1e4e40bc98afd15b36036db
10240418 <liu2021632245@163.com>
2024-08-23 17:02:05 +0800

    mete turn on or turn off

--- src/pages/deviceManage/meter/widgets/meterDialog.vue
+++ src/pages/deviceManage/meter/widgets/meterDialog.vue
@@ -4,7 +4,12 @@
       <!-- chart -->
       <!-- <span class="text-[20px] font-bold">Read Meter Logs</span> -->
       <div class="flex min-h-[100px] h-[180px] mr-2">
-        <VaChart :data="chartData" class="h-24" type="line" :options="options" />
+        <VaChart
+          :data="chartData"
+          class="h-24"
+          type="line"
+          :options="options"
+        />
       </div>
       <!-- top -->
       <div class="flex flex-row justify-between items-center">
@@ -14,18 +19,30 @@
             class="w-[20px] h-[20px] rounded-full border border-solid shadow-lg"
             :class="{ 'bg-green-600': meterStatus === true, 'bg-red-500': meterStatus !== true }"
           ></div>
-          <span v-if="meterStatus === true" class="text-[15px] font-bold">开启</span>
-          <span v-if="meterStatus !== true" class="text-[15px] font-bold">关闭</span>
+          <span
+            v-if="meterStatus === true"
+            class="text-[15px] font-bold"
+          >开启</span>
+          <span
+            v-if="meterStatus !== true"
+            class="text-[15px] font-bold"
+          >关闭</span>
         </div>
       </div>
       <div class="flex flex-row justify-between">
-        <DetailCard :labels="labelsProp" :datas="datasProp" :font-weight="`font-bold`" :index-weight="3">
+        <DetailCard
+          :labels="labelsProp"
+          :datas="datasProp"
+          :font-weight="`font-bold`"
+          :index-weight="3"
+        >
           <div class="flex flex-col relative">
             <!-- Gateway Section -->
             <div class="flex flex-row w-full items-center">
               <VaListLabel class="flex justify-start">Gateway</VaListLabel>
               <div class="absolute ml-[95px]">
-                <span class="truncate w-[72px]">{{ meter?.gateway?.name }}</span>
+                <span
+                  class="truncate w-[72px]">{{ meter?.gateway?.name }}</span>
               </div>
               <VaPopover
                 class="absolute ml-52"
@@ -39,16 +56,20 @@
                   @click="isGatewayCollapsed = !isGatewayCollapsed"
                 />
                 <template #body>
-                  <div class="grid grid-cols-2 gap-2 border border-solid p-2 rounded-md shadow-lg">
+                  <div
+                    class="grid grid-cols-2 gap-2 border border-solid p-2 rounded-md shadow-lg"
+                  >
                     <VaListLabel class="flex justify-start">ID</VaListLabel>
                     <span>{{ meter?.gateway.id }}</span>
                     <VaListLabel class="flex justify-start">Name</VaListLabel>
                     <span>{{ meter?.gateway.name }}</span>
-                    <VaListLabel class="flex justify-start">IP Address </VaListLabel>
+                    <VaListLabel class="flex justify-start">IP Address
+                    </VaListLabel>
                     <span>{{ meter?.gateway.ipAddr }}</span>
                     <VaListLabel class="flex justify-start">Remark</VaListLabel>
                     <span>{{ meter?.gateway.remark }}</span>
-                    <VaListLabel class="flex justify-start">Created At </VaListLabel>
+                    <VaListLabel class="flex justify-start">Created At
+                    </VaListLabel>
                     <span>{{ meter?.gateway.createdAt }}</span>
                   </div>
                 </template>
@@ -59,7 +80,8 @@
             <div class="flex flex-row mr-3 items-center gap-3">
               <VaListLabel class="flex justify-start">Unit</VaListLabel>
               <div class="absolute ml-[95px]">
-                <span class="truncate w-[72px] text-[16px]">{{ meter?.unit?.unit }}</span>
+                <span
+                  class="truncate w-[72px] text-[16px]">{{ meter?.unit?.unit }}</span>
               </div>
               <VaPopover
                 class="absolute ml-52"
@@ -73,7 +95,9 @@
                   @click="isUnitCollapsed = !isUnitCollapsed"
                 />
                 <template #body>
-                  <div class="grid grid-cols-[1fr_4fr] border border-solid rounded-md shadow-lg p-2">
+                  <div
+                    class="grid grid-cols-[1fr_4fr] border border-solid rounded-md shadow-lg p-2"
+                  >
                     <VaListLabel class="flex justify-start">ID</VaListLabel>
                     <span>{{ meter?.unit.id }}</span>
                     <VaListLabel class="flex justify-start">Floor</VaListLabel>
@@ -83,7 +107,8 @@
 
                     <VaListLabel class="flex justify-start">Remark</VaListLabel>
                     <span>{{ meter?.unit.remark }}</span>
-                    <VaListLabel class="flex justify-start">Created At </VaListLabel>
+                    <VaListLabel class="flex justify-start">Created At
+                    </VaListLabel>
                     <span>{{ meter?.unit.createdAt }}</span>
                   </div>
                 </template>
@@ -122,7 +147,12 @@
           </div>
         </DetailCard>
         <div class="flex flex-col justify-end items-end mt-[0px] mr-6">
-          <VaButton preset="secondary" border-color="primary" class="h-[26px] w-[82px]" @click="openEditModal">
+          <VaButton
+            preset="secondary"
+            border-color="primary"
+            class="h-[26px] w-[82px]"
+            @click="openEditModal"
+          >
             <span class="text-[14px]">Edit</span>
           </VaButton>
         </div>
@@ -137,7 +167,7 @@
     <div class="dialog-footer flex flex-row gap-2 mr-2">
       <div class="flex flex-row items-center justify-center gap-5">
         <VaButton
-          v-if="meterStatus === true"
+          v-if="meterStatus === false"
           color="primary"
           :loading="isOperating ? true : false"
           class="h-[30px] w-[92px] text-nowrap"
@@ -146,7 +176,7 @@
           <span class="text-[14px]">Turn ON</span>
         </VaButton>
         <VaButton
-          v-if="meterStatus === false"
+          v-if="meterStatus === true"
           color="primary"
           :loading="isOperating ? true : false"
           class="h-[30px] w-[92px] text-nowrap"
@@ -155,12 +185,23 @@
           <span class="text-[12px]">Turn OFF</span>
         </VaButton>
       </div>
-      <VaButton color="primary" :loading="isOperating ? true : false" class="h-[30px] w-[92px]" @click="read">
+      <VaButton
+        color="primary"
+        :loading="isOperating ? true : false"
+        class="h-[30px] w-[92px]"
+        @click="read"
+      >
         <span class="text-[14px]">RealTime</span>
       </VaButton>
     </div>
     <!-- edit modal -->
-    <VaModal v-model="showEditModal" size="small" mobile-fullscreen close-button hide-default-actions>
+    <VaModal
+      v-model="showEditModal"
+      size="small"
+      mobile-fullscreen
+      close-button
+      hide-default-actions
+    >
       <h1>Edit Meter</h1>
       <EditMeterForm
         :model-value="meter"
@@ -169,9 +210,20 @@
         @close="closeEditModal"
       />
     </VaModal>
-    <VaModal v-model="showReadModal" size="small" mobile-fullscreen close-button hide-default-actions>
-      <div v-if="meter?.type === 1" class="flex flex-col p-4">
-        <h1 class="text-xl font-bold mb-4 flex flex-row items-start justify-start">Status</h1>
+    <VaModal
+      v-model="showReadModal"
+      size="small"
+      mobile-fullscreen
+      close-button
+      hide-default-actions
+    >
+      <div
+        v-if="meter?.type === 1"
+        class="flex flex-col p-4"
+      >
+        <h1
+          class="text-xl font-bold mb-4 flex flex-row items-start justify-start"
+        >Status</h1>
         <div class="space-y-2 w-full">
           <div class="flex justify-between">
             <VaListLabel class="flex justify-start">Voltage</VaListLabel>
@@ -203,8 +255,13 @@
           </div>
         </div>
       </div>
-      <div v-else class="flex flex-col p-4">
-        <h1 class="text-xl font-bold mb-4 flex justify-start items-start ml-[-2px]">Status</h1>
+      <div
+        v-else
+        class="flex flex-col p-4"
+      >
+        <h1
+          class="text-xl font-bold mb-4 flex justify-start items-start ml-[-2px]"
+        >Status</h1>
         <div class="space-y-2 w-full">
           <div class="flex justify-between">
             <VaListLabel class="flex justify-start">Volume</VaListLabel>
